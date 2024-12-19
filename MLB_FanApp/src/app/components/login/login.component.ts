89179c9e82f6999bdc35b357426d07a6146bd224
shanpatel371 <85136600+shanpatel371@users.noreply.github.com>
2024-12-18 21:08:17 -0800

    set up google authentication

---
+++ MLB_FanApp/src/app/components/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  onSubmit() {
+    // Replace this with actual authentication logic
+    console.log('Form submitted');
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MLB_GCH_FanApp
+a interactive MLB fan experience app to do random swipes on players 
